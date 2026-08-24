42b4642a955cdb2dce0ee96a5e74c4fa906c46db
davidllano95 <davidllano95@icloud.com>
2026-08-23 20:46:26 -0500

    Bump Show Day runtime asset version

--- showday-edge.js
+++ showday-edge.js
@@ -1,4 +1,4 @@
-const SHOWDAY_RUNTIME_VERSION = "20260823-2";
+const SHOWDAY_RUNTIME_VERSION = "20260823-3";
 
 const SHARED_PUBLIC_HEADER_HTML = `
 <header class="site-header" id="siteHeader" data-sdlive-shared-public-header>
