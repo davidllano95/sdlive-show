a1b25041eac9a330206ea545e49b67e71b400e68
davidllano95 <davidllano95@icloud.com>
2026-08-25 15:50:33 -0500

    Bust testimonial runtime cache

--- showday-edge.js
+++ showday-edge.js
@@ -1,5 +1,5 @@
 const SHOWDAY_RUNTIME_VERSION = "20260824-2";
-const PUBLIC_AUDIT_RUNTIME_VERSION = "20260825-2";
+const PUBLIC_AUDIT_RUNTIME_VERSION = "20260825-3";
 
 const SHARED_PUBLIC_HEADER_HTML = `
 <header class="site-header" id="siteHeader" data-sdlive-shared-public-header>
